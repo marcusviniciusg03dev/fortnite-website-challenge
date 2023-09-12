76c58f9db3034bc992da73c5220a864049b286d1
Marcus Vinicius Gomes <marcusvinid3v@gmail.com>
2023-09-12 14:24:09 -0300

    Chore: Initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,56 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      spacing: {
+        '1': '4px',
+        '2': '8px',
+        '3': '12px',
+        '4': '16px',
+        '5': '20px',
+        '6': '24px',
+        '7': '28px',
+        '8': '32px',
+        '10': '40px',
+        '12': '48px',
+        '16': '64px',
+        '20': '80px',
+        '24': '96px',
+        '32': '128px',
+        '40': '160px',
+        '48': '192px',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        blue: {
+          '600': '#0866B0',
+        },
+        cyan: {
+          '400': '#46CAE9'
+        },
+        yellow: {
+          '200': '#FEFF04',
+          '400': '#F5E401'
+        },
+        grey: {
+          '50': '#F9FAFB',
+          '100': '#F3F4F6',
+          '200': '#E5E7EB',
+          '300': '#D1D5DB',
+          '400': '#9CA3AF',
+          '500': '#6B7280',
+          '600': '#4B5563',
+          '700': '#374151',
+          '800': '#1F2937',
+          '900': '#111827',
+          '950': '#030712',
+        },
+        white: '#FFFFFF',
+        black: '#000000',
+      }
     },
   },
   plugins: [],
