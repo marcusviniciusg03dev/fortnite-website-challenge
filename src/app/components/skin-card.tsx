3b0f5fb07fbd0f6bbacc0fe04db61f0fd7bb8c0b
Marcus Vinicius Gomes <marcusvinid3v@gmail.com>
2023-09-26 12:53:15 -0300

    Style: Add skin card animation on hover

--- src/app/components/skin-card.tsx
+++ src/app/components/skin-card.tsx
@@ -12,10 +12,10 @@ export default function SkinCard({ imageUrl, title }: SkinCardProps) {
                 <img
                     src={imageUrl}
                     alt="skin"
-                    className="absolute block z-10 bottom-5 left-0 right-0 h-[105%] w-auto m-auto"
+                    className="absolute block z-10 bottom-5 left-0 right-0 h-[105%] w-auto m-auto group-hover:translate-x-2 transition-all duration-150"
                 />
                 <div
-                    className="invisible group-hover:visible duration-200 transition-all flex flex-col justify-center p-10 gap-2 h-1/2 bg-gradient-to-t from-[#090D15] to-[#090D1500] w-full bottom-0 z-20 mt-auto"
+                    className="opacity-0 group-hover:opacity-100 duration-400 transition-all flex flex-col justify-center p-10 gap-2 h-1/2 bg-gradient-to-t from-[#090D15] to-[#090D1500] w-full bottom-0 z-20 mt-auto"
                 >
                     <h4 className="text-4xl font-logo uppercase opacity-0 group-hover:opacity-100 transition-all duration-800">{title}</h4>
                     <p className="text-xl opacity-0 group-hover:opacity-100 transition-all duration-800">Capítulo 2, Temporada 5</p>
